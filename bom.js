f5a6a02e675d9209bfb73cd108eb6640c7295353
Andy Terwilliger <andy@terwilliger1@gmail.com>
2021-12-16 10:11:49 -0600

    styling on BoM page and attached links

--- bom.js
+++ bom.js
@@ -1,3 +1,15 @@
 document.getElementById("home").onclick= function(){
     location.href= "index.html";
 }
+
+document.getElementById("ballast").onclick= function(){
+    location.href= "https://ballastpoint.com/beer/grapefruit-sculpin/";
+}
+
+document.getElementById("revolution").onclick= function(){
+    location.href= "https://revbrew.com/beer/seasonals/sun-crusher";
+}
+
+document.getElementById("bodem").onclick= function(){
+    location.href= "https://www.halfacrebeer.com/bodem"
+}
